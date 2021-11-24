2029076f952400122753bfa7f890abf931967177
chanbito <gabrielmarafiga@hotmail.com>
2021-11-24 15:12:47 -0300

    criado novos end-points

--- ProjetoEngenhariaSoftware/Controllers/HomeController.cs
+++ ProjetoEngenhariaSoftware/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Swashbuckle.Swagger.Annotations;
+﻿using ProjetoEngenhariaSoftware.Helpers;
+using ProjetoEngenhariaSoftware.Models;
+using Swashbuckle.Swagger.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,12 +20,46 @@ namespace ProjetoEngenhariaSoftware.Controllers
         /// <remarks>AccessLevel is not a required</remarks>
         [HttpGet]
         [AllowAnonymous]
-        [SwaggerResponse(HttpStatusCode.OK, "Return a list of pets", typeof(string))]
-        [SwaggerResponse(HttpStatusCode.InternalServerError, "Exception Custom Object with message about error in ParkingException.Message", typeof(Exception))]
+        [SwaggerResponse(HttpStatusCode.OK, "Return a list of pets", typeof(List<PetClass>))]
         [SwaggerResponse(HttpStatusCode.Unauthorized, "Custom exception indicating that api token is not valid or has not been reported", typeof(Exception))]
-        public IHttpActionResult GetPets()
+        public IHttpActionResult GetAllPets()
         {
-            return Content(HttpStatusCode.OK, "pets");
+            return Content(HttpStatusCode.OK, Banco.selectAll());
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [SwaggerResponse(HttpStatusCode.OK, "Return the id from this pet", typeof(int))]
+        [SwaggerResponse(HttpStatusCode.Unauthorized, "Custom exception indicating that api token is not valid or has not been reported", typeof(Exception))]
+        public IHttpActionResult InsertPets(PetClass Pet)
+        {
+            return Content(HttpStatusCode.OK, Banco.insert(Pet));
+        }
+
+        [HttpPut]
+        [AllowAnonymous]
+        [SwaggerResponse(HttpStatusCode.OK, "Return a confirmation to update", typeof(bool))]
+        [SwaggerResponse(HttpStatusCode.Unauthorized, "Custom exception indicating that api token is not valid or has not been reported", typeof(Exception))]
+        public IHttpActionResult UpdatePets([FromUri]int id, [FromBody] PetClass pet)
+        {
+            return Content(HttpStatusCode.OK, Banco.update(pet,id));
+        }
+
+        [HttpDelete]
+        [AllowAnonymous]
+        [SwaggerResponse(HttpStatusCode.OK, "Return a list of pets", typeof(bool))]
+        [SwaggerResponse(HttpStatusCode.Unauthorized, "Custom exception indicating that api token is not valid or has not been reported", typeof(Exception))]
+        public IHttpActionResult DeletePet(int id)
+        {
+            try
+            {
+                Banco.delete(id);
+                return Content(HttpStatusCode.OK, true);
+            }
+            catch
+            {
+                return Content(HttpStatusCode.OK, false);
+            }
         }
     }
 }
